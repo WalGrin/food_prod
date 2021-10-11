4e174f3dc82b500c5306a2831b90011d3bb1da82
ValeryGrinko <grinko.vr@intechs.by>
2021-10-11 16:39:05 +0300

    fetch formData request

--- js/script.js
+++ js/script.js
@@ -233,29 +233,27 @@ window.addEventListener('DOMContentLoaded', () => {
             `;
             form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-            request.setRequestHeader('Content-type', 'application/json')
-
             const formData = new FormData(form);
-            const object = {};
-            formData.forEach(function (value, key) {
-                object[key] = value;
-            });
+            // const object = {};
+            // formData.forEach(function (value, key) {
+            //     object[key] = value;
+            // });
+            //
+            // const json = JSON.stringify(object);
 
-            const json = JSON.stringify(object);
-
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showModal(message.success);
-                    form.reset();
-                    statusMessage.remove();
-                } else {
-                    showModal(message.failure);
-                }
+            fetch('server.php', {
+                method: "POST",
+                // headers: {'Content-type': 'application/json'},
+                body: formData
+            }).then(data => data.text())
+                .then(data => {
+                console.log(data);
+                showModal(message.success);
+                statusMessage.remove();
+            }).catch(() => {
+                showModal(message.failure);
+            }).finally(() => {
+                form.reset();
             })
         });
     }
