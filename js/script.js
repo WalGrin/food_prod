4f04805125decdcf965c03fb2fa5f90ea118165d
ValeryGrinko <grinko.vr@intechs.by>
2021-10-11 16:57:13 +0300

    fetch json request

--- js/script.js
+++ js/script.js
@@ -234,17 +234,15 @@ window.addEventListener('DOMContentLoaded', () => {
             form.insertAdjacentElement('afterend', statusMessage);
 
             const formData = new FormData(form);
-            // const object = {};
-            // formData.forEach(function (value, key) {
-            //     object[key] = value;
-            // });
-            //
-            // const json = JSON.stringify(object);
+            const object = {};
+            formData.forEach(function (value, key) {
+                object[key] = value;
+            });
 
             fetch('server.php', {
                 method: "POST",
-                // headers: {'Content-type': 'application/json'},
-                body: formData
+                headers: {'Content-type': 'application/json'},
+                body: JSON.stringify(object)
             }).then(data => data.text())
                 .then(data => {
                 console.log(data);
